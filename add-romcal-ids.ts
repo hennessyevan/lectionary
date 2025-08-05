0cbfd041679c8ed1e1edef759a078d0e4f938df4
Evan Hennessy <git@hennessyevan.com>
2025-08-05 15:54:12 -0400

    use romcal ids for datse

--- add-romcal-ids.ts
+++ add-romcal-ids.ts
@@ -3,7 +3,7 @@ import { Canada_En } from '@romcal/calendar.canada'
 import { DateTime } from 'luxon'
 import { readFile, writeFile } from 'node:fs/promises'
 import path from 'node:path'
-import lwcJSON from './assets/lwc.json' assert { type: 'json' }
+import lwcJSON from './assets/canada-lectionary-raw.json' assert { type: 'json' }
 
 const romcal = new Romcal({
   localizedCalendar: Canada_En,
@@ -11,11 +11,14 @@ const romcal = new Romcal({
 
 let calendars: Record<number, LiturgicalCalendar> = {}
 
+const newJSON: Record<string, any> = {}
+
 async function addRomcalIds() {
   for (const [date, item] of Object.entries(lwcJSON)) {
     const dateTime = DateTime.fromISO(date)
 
     if (!calendars.hasOwnProperty(dateTime.year)) {
+      console.log(dateTime.year, 'not found in calendars, generating...')
       calendars[dateTime.year] = await romcal.generateCalendar(dateTime.year)
     }
 
@@ -28,20 +31,21 @@ async function addRomcalIds() {
       break
     }
 
-    lwcJSON[date] = {
+    newJSON[romcalId] = {
       ...item,
       id: romcalId,
     }
   }
 
   await writeFile(
-    path.join('./assets/lwc.json'),
-    JSON.stringify(lwcJSON, null, 2)
+    path.join('./assets/canada-lectionary-raw.json'),
+    JSON.stringify(newJSON, null, 2)
   )
-  console.info('Romcal IDs added to lwc.json')
-  console.info(`Total entries processed: ${Object.keys(lwcJSON).length}`)
+
+  console.info('Romcal IDs added to canada-lectionary-raw.json')
+  console.info(`Total entries processed: ${Object.keys(newJSON).length}`)
   console.info(`Total years processed: ${Object.keys(calendars).length}`)
   console.info('Done!')
 }
 
-// addRomcalIds()
+addRomcalIds()
